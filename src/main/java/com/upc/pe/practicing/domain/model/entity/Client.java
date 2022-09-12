909c0d317a162c704b9cb8fd5127886c837a4b0f
2CTO <u201521691@upc.edu.pe>
2022-09-12 09:49:45 -0500

    shared folders were created and POM was actualized

--- src/main/java/com/upc/pe/practicing/domain/model/entity/Client.java
+++ src/main/java/com/upc/pe/practicing/domain/model/entity/Client.java
@@ -1,4 +1,16 @@
 package com.upc.pe.practicing.domain.model.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
 public class Client {
 }
